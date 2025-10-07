d25d7c0b6f00e49bc615c21148c4d0c9b14320ce
Shanatanu Kale <kaleshantanu2260@gmail.com>
2025-10-07 23:14:11 +0530

    Added full LinkedIn project source code

--- posts-service/src/main/java/com/shantanux0/linkdin/post_service/PostsServiceApplication.java
+++ posts-service/src/main/java/com/shantanux0/linkdin/post_service/PostsServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PostServiceApplication {
+public class PostsServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PostServiceApplication.class, args);
+		SpringApplication.run(PostsServiceApplication.class, args);
 	}
 
 }
